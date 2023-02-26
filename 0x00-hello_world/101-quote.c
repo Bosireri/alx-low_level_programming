17d32b860e77f94b205737ce2093966f33b20343
Bosireri <bosireri24@gmail.com>
2023-02-26 04:09:18 -0800

    0x00-hello-world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
 * main - A c program that brings a line to the standard error
 * Return 1 (Success)
 */
-int main (void)
+int main(void)
 {
 fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
