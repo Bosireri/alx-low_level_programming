4f335e83822410d4cc314fd708a1234f23e44ff0
Bosireri <bosireri24@gmail.com>
2023-03-02 01:42:50 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+char alpa;
+
+for (alpa = 'a'; alpa <= 'z' ; alpa++)
+putchar(alpa);
+putchar('\n');
+
+return (0);
+}
