2b83835f4e37ad4d700e5d73754fb91375bb87aa
Bosireri <bosireri24@gmail.com>
2023-03-02 02:22:51 -0800

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 7-print_tebahpla.c -o 7-print_tebahplagcc -Wall -pedantic -Werror -Wextra -std=gnu89 7-print_tebahpla.c -o 7-print_tebahpla git add . pwd ..

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpa;
+
+	for (alpa = 'z' ; alpa >= 'a' ; alpa--)
+	putchar(alpa);
+	putchar('\n');
+	return (0);
+}
