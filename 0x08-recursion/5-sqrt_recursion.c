d650a89b0b5f6be5028c09d1ec2e396f84928c88
Bosireri <bosireri24@gmail.com>
2023-03-09 05:21:01 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,40 +1,36 @@
 #include "main.h"
-
-/*Declaration Helper function*/
-int is_sqrt(int n, int m);
-
+#include <stdio.h>
+#include <string.h>
 /**
- * _sqrt_recursion- entry point
- *
- * @n: integer variable
- * Return: square root of n or -1 (!Perfect sqrt)
+ * power_operation - returns the natural square root of a number.
+ * @n: input number.
+ * @c: iterator.
+ * Return: square root or -1.
+ */
+int power_operation(int n, int c)
+{
+	if (c % (n / c) == 0)
+	{
+		if (c * (n / c) == n)
+			return (c);
+		else
+			return (-1);
+	}
+	return (0 + power_operation(n, c + 1));
+}
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: input number.
+ * Return: natural square root.
  */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-	{
 		return (-1);
-	}
-	return (is_sqrt(n, n / 2));
-}
-
-/**
- *is_sqrt - helper function
- *
- * @n: integer variable to be checked
- * @m: integer variable (n/2)
- * Return: square root or -1 (!Perfect sqrt)
- */
-int is_sqrt(int n, int m)
-{
 	if (n == 0)
 		return (0);
-	else if (n == 1)
+	if (n == 1)
 		return (1);
-	else if (m < 1)
-		return (-1);
-	else if (m * m == n)
-		return (m);
-	return (is_sqrt(n, m - 1));
+	return (power_operation(n, 2));
 }
 
