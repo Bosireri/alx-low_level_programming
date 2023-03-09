bec1482424e65db7a14c627e51ba61ff494f01d2
Bosireri <bosireri24@gmail.com>
2023-03-09 00:46:26 -0800

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * factorial - entry point
+ *
+ * @n: integer variable
+ * Return: factorial
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
+
