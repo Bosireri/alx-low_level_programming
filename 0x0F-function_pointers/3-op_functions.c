20356f2bbdac36a559fd3b97171c59c10988eba5
Bosireri <bosireri24@gmail.com>
2023-03-24 01:03:11 -0700

    3[main.c, op functions.c, get op func.c, calc.h]

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ * op_add - entry point
+ *
+ * @a: arg 1 (integer variable)
+ * @b: arg 2 (integer variable)
+ *
+ * Return: result of operation
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - entry point
+ *
+ * @a: arg 1 (integer variable)
+ * @b: arg 2 (integer variable)
+ *
+ * Return: result of operation
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - entry point
+ *
+ * @a: arg 1 (integer variable)
+ * @b: arg 2 (integer variable)
+ *
+ * Return: result of operation
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - entry point
+ *
+ * @a: arg 1 (integer variable)
+ * @b: arg 2 (integer variable)
+ *
+ * Return: result of operation
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - entry point
+ *
+ * @a: arg 1 (integer variable)
+ * @b: arg 2 (integer variable)
+ *
+ * Return: result of operation
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
